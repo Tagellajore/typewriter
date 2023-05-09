dd6018988a58b411f181740adf45c3a1b1ec46d7
Tagellajore <tagelsft9@gmail.com>
2023-05-09 13:50:33 -0400

    displayed a sentence as an animation

---
+++ typewriter.js
@@ -0,0 +1,16 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 0
+for (let i = 0; i < sentence.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(sentence[i]);
+    
+  }, delay);
+  delay = 50*(i + 1)
+}
+ 
+setTimeout(() => {
+  console.log('')
+}, delay)
+
+
+// console.log('')
